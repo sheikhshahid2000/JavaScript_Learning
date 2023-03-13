84119395d0a2fc5411abc3afa72ab047fc9d5586
sheikhshahid2000 <73874196+sheikhshahid2000@users.noreply.github.com>
2023-03-13 12:40:11 +0530

    func2 Added

---
+++ functions/fun2.js
@@ -0,0 +1,10 @@
+
+  function mobiles(mobile,ram){
+    mobile=" Note 5";
+    console.log(mobile +" " + ram  );
+
+  }
+  let name="Realme 4g";
+  mobiles(name,"6Gb");
+  console.log(name);
+  // in above program we are modifying the outer variable inside the function.but the variable is only changed inside the functiion.cd
