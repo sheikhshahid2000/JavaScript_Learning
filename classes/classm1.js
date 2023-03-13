6ddadffe97d2f0a495882cb0608a66fb248028af
sheikhshahid2000 <73874196+sheikhshahid2000@users.noreply.github.com>
2023-03-13 11:37:32 +0530

    first time Upload

---
+++ classes/classm1.js
@@ -0,0 +1,14 @@
+class Room{
+    constructor(
+        roomno,
+        capacity,
+        color)
+    {
+        this.roomno=roomno;
+        this.capacity=capacity;
+        this.color=color;
+    }
+
+    doorStatus(door)
+}
+export default Room;
---
+++ classes/classm2.js
@@ -0,0 +1,6 @@
+import Room from "./classm1.js";
+
+const classRoom=new Room(201,
+    100,"white");
+    
+console.log(classRoom);
